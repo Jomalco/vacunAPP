bd8bb6f0c14346cd0afe14c61e01dad3532a6f74
José Manuel Alvarez Colombo <jmacmdp@gmail.com>
2023-11-22 19:04:06 -0300

    Trying changes

--- src/scripts/inicializarVacunas.js
+++ src/scripts/inicializarVacunas.js
@@ -18,6 +18,38 @@ function calculateAgeInMonths(birthdate) {
     const ageInMonths = Math.floor(ageInMilliseconds / millisecondsInMonth);
     return ageInMonths;
 }
+
+function calculateVaccinationDate(birthdate, vaccineAgeInMonths) {
+    // Split the birthdate into day, month, and year
+    var [day, month, year] = birthdate.split("/");
+    
+    // JavaScript counts months from 0, so we subtract 1 from the month
+    month--;
+    
+    // Create a new Date object with the birthdate
+    var birthDate = new Date(year, month, day);
+    
+    var currentAgeInMonths = calculateAgeInMonths(birthDate);
+    var monthsUntilVaccine = vaccineAgeInMonths - currentAgeInMonths;
+    
+    var currentYear = new Date().getFullYear();
+    var currentMonth = new Date().getMonth();
+    var currentDate = new Date().getDate();
+    
+    var vaccineYear = currentYear;
+    var vaccineMonth = currentMonth + monthsUntilVaccine;
+    
+    // Check if the month overflowed into the next year
+    if (vaccineMonth > 11) {
+      vaccineYear++;
+      vaccineMonth = vaccineMonth - 12;
+    }
+    
+    var vaccinationDate = new Date(vaccineYear, vaccineMonth, currentDate);
+    return formatDate(vaccinationDate);
+  }
+  
+
 export default function inicializarVacunas(birthdate) {
     const bd = calculateAgeInMonths(birthdate);
     
