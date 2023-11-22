537e5590f66ffb3aab49fe31f60b1a08152f16cc
José Manuel Alvarez Colombo <jomalcomania@gmail.com>
2023-11-22 18:56:02 -0300

    Started creating the initialization of the vaccine arrays, added Neumococo Conjugada

--- src/scripts/completarArrayVacunas.js
+++ src/scripts/completarArrayVacunas.js
@@ -1,79 +1,88 @@
-export default function grupoDos(age) {
+var vacunasPasadas = [];
+var vacunasCercanas = [];
+var vacunasFuturas = [];
 
-    
+function calculateAgeInMonths(birthdate) {
+    if (typeof birthdate !== 'string') {
+         birthdate = birthdate.toString();
+     }
+    const parts = birthdate.split("/");
+    const formattedBirthdate = `${parts[1]}/${parts[0]}/${parts[2]}`;
+    const date = new Date(formattedBirthdate);
+    const now = new Date();
+    const diffInMilliseconds = now - date;
+    const ageInMonths = Math.round(diffInMilliseconds / (2592000000));
+    return ageInMonths;
 }
 
-function BCG(age) {
+function calculateVaccinationDate(birthdate, vaccineAgeInMonths) {
+    // Split the birthdate into day, month, and year
+    var [day, month, year] = birthdate.split("/");
     
-    return vaccineStatus;
-}
+    // JavaScript counts months from 0, so we subtract 1 from the month
+    month--;
+    
+    // Create a new Date object with the birthdate
+    var birthDate = new Date(year, month, day);
+    
+    var currentAgeInMonths = calculateAgeInMonths(birthdate);
+    var monthsUntilVaccine = vaccineAgeInMonths - currentAgeInMonths;
+    
+    var currentYear = new Date().getFullYear();
+    var currentMonth = new Date().getMonth();
+    var currentDate = new Date().getDate();
+    
+    var vaccineYear = currentYear;
+    var vaccineMonth = currentMonth + monthsUntilVaccine;
+    
+    // Check if the month overflowed into the next year
+    if (vaccineMonth > 11) {
+      vaccineYear++;
+      vaccineMonth = vaccineMonth - 12;
+    }
+    
+    var vaccinationDate = new Date(vaccineYear, vaccineMonth, currentDate);
+    
+    // Format the date as "DD/MM/YYYY"
+    var day = vaccinationDate.getDate();
+    var month = vaccinationDate.getMonth() + 1;
+    var year = vaccinationDate.getFullYear();
+    
+    // Add leading zeros to day and month if they are less than 10
+    if (day < 10) day = '0' + day;
+    if (month < 10) month = '0' + month;
+    
+    return day + '/' + month + '/' + year;
+ }
 
-function HepatitisB(age) {
-    
-    return vaccineStatus;
-}
+//----------------------------------------------------------------      Vacunas
+class NeumococoConjugada {
+    constructor(birthdate) {
+      this.birthdate = birthdate;
+      this.NeumococoConjugada = 
+      {
+        nombre: "Neumococo Conjugada",
+        status: 3,
+        factoresDeRiesgo: [1,4],
+        id: 24,
+        dosis:[0,3],
+        separacionDosis:[0,2,8],
+        fechaVacunacion: this.calcularFechaVacunacion()
+      };
+    }
+    calcularFechaVacunacion() {
+        if(calculateAgeInMonths(this.birthdate) <= 2)
+        {
+            return calculateVaccinationDate(this.birthdate, 2)
+        }
+        else 
+        {
+            return calculateVaccinationDate(this.birthdate, 18*12)
+        }
+    }
+   }
 
-function Neumococo(age) {
-    
-    return vaccineStatus;
-}
+export default function completarArrayVacunas(birthdate) {
 
-function Pentavalente(age) {
     
-    return vaccineStatus;
-}
-
-function IPV(age) {
-    
-    return vaccineStatus;
-}
-
-function Rotavirus(age) {
-    
-    return vaccineStatus;
-}
-
-function Meningococo(age) {
-    
-    return vaccineStatus;
-}
-
-function Antigripal(age) {
-    
-    return vaccineStatus;
-}
-
-function HepatitisA(age) {
-    
-    return vaccineStatus;
-}
-
-function TripleViral(age) {
-    
-    return vaccineStatus;
-}
-
-function Varicela(age) {
-    
-    return vaccineStatus;
-}
-
-function TripleBacterianaCelular(age) {
-    
-    return vaccineStatus;
-}
-
-function TripleBacterianaAcelular(age) {
-    
-    return vaccineStatus;
-}
-
-function VirusPapilomaHumano(age) {
-    
-    return vaccineStatus;
-}
-
-function DobleBacteriana(age) {
-    
-    return vaccineStatus;
 }
