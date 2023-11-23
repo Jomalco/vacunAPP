ff3c2bea5e290f72bcd5dd522df93bbb040a2699
José Manuel Alvarez Colombo <jmacmdp@gmail.com>
2023-11-23 14:55:56 -0300

    Added vaccines

--- src/scripts/completarArrayVacunas.js
+++ src/scripts/completarArrayVacunas.js
@@ -71,16 +71,86 @@ class NeumococoConjugada {
       };
     }
     calcularFechaVacunacion() {
-        if(calculateAgeInMonths(this.birthdate) <= 2)
-        {
-            return calculateVaccinationDate(this.birthdate, 2)
-        }
-        else 
-        {
-            return calculateVaccinationDate(this.birthdate, 18*12)
-        }
+      if(calculateAgeInMonths(this.birthdate) <= 2)
+      {
+          return calculateVaccinationDate(this.birthdate, 2)
+      }
+      else 
+      {
+          return calculateVaccinationDate(this.birthdate, 18*12)
+      }
     }
-   }
+}
+
+class Pentavalente {
+  constructor(birthdate) {
+    this.birthdate = birthdate;
+    this.Pentavalente = 
+      {
+        nombre: "Quíntuple/Pentavalente",
+        status: 3,
+        factoresDeRiesgo: [],
+        id: 25,
+        dosis:[0,4],
+        separacionDosis:[0,2,2,9],
+        fechaVacunacion: calculateVaccinationDate(this.birthdate, 2)
+      };
+    }
+}
+
+class IPV {
+  constructor(birthdate) {
+    this.birthdate = birthdate;
+    this.IPV = 
+      {
+        nombre: "IPV",
+        status: 3,
+        factoresDeRiesgo: [],
+        id: 26,
+        dosis:[0,4],
+        separacionDosis:[0,2,2,54],
+        fechaVacunacion: calculateVaccinationDate(this.birthdate, 2)
+      };
+    }
+}
+
+class Rotavirus {
+  constructor(birthdate) {
+    this.birthdate = birthdate;
+    this.Rotavirus = 
+      {
+        nombre: "Rotavirus",
+        status: 3,
+        factoresDeRiesgo: [],
+        id: 27,
+        dosis:[0,2],
+        separacionDosis:[0,2],
+        fechaVacunacion: calculateVaccinationDate(this.birthdate, 2)
+      };
+    }
+}
+
+class Meningococo {
+  constructor(birthdate) {
+    this.birthdate = birthdate;
+    this.Meningococo = 
+      {
+        nombre: "Meningococo ACYW",
+        status: 3,
+        factoresDeRiesgo: [],
+        id: 28,
+        dosis:[0,3],
+        separacionDosis:[0,2,10],
+        fechaVacunacion: calculateVaccinationDate(this.birthdate, 3)
+      };
+    }
+    calcularFechaVacunacion() {
+      if(calculateAgeInMonths(this.birthdate) <= 2)
+      {
+
+      }
+    }
+}
 
 export default function completarArrayVacunas(birthdate) {
 
