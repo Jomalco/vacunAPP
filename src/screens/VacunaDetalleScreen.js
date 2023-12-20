fa2590f8937b573f962a9c0c1ccf47086019ed46
José Manuel Alvarez Colombo <jmacmdp@gmail.com>
2023-12-19 21:12:13 -0300

    Changes to the vaccine details screen

--- src/screens/VacunaDetalleScreen.js
+++ src/screens/VacunaDetalleScreen.js
@@ -1,11 +1,32 @@
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity } from 'react-native'
+import { PersonaContext } from '../contexts/PersonaContext'
+import React, { useContext, useState } from 'react'
 
-const VacunaDetalleScreen = (props) => {
+const VacunaDetalleScreen = (paramprops) => {
+    const {
+        index
+      } = useContext(PersonaContext)
+
+    let props = paramprops.route.params.item
+    console.log(props)
     return (
         <SafeAreaView style={styles.container}>
             <Text style={styles.text}>{props.nombre}</Text>
             <Text style={styles.textTwo}>Día de vacunación estimado: {props.fechaVacunacion}</Text>
+            <Text style={styles.textTwo}>Dosis {props.dosis[0]+1}/{props.dosis[1]}</Text>
+            <View style={{flexDirection: "row", marginTop: 10, alignContent: "center", alignItems: "center"}}>
+                <TouchableOpacity style={styles.dosisButton}>
+                    <Text>
+                        -
+                    </Text>
+                </TouchableOpacity>
+                    <Text>Cambiar dosis</Text>
+                <TouchableOpacity style={styles.dosisButton}>
+                    <Text>
+                        +
+                    </Text>
+                </TouchableOpacity>
+            </View>
         </SafeAreaView>
       );
     };
@@ -13,13 +34,30 @@ const VacunaDetalleScreen = (props) => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        width: "100%",
+        alignContent: "center",
+        alignItems: "center",
+        paddingTop: 20,
     },
     text: {
-        fontSize: 25,
+        fontSize: 35,
         fontWeight: '500',
     },
     textTwo: {
-      fontStyle: "italic"
+        fontSize: 15,
+        fontStyle: "italic",
+        paddingTop: 5
+    },
+    dosisButton: {
+        width: 40,
+        height: 40,
+        borderRadius: 25,
+        backgroundColor: "lightgrey",
+        borderColor: "grey",
+        borderWidth: 2,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginHorizontal: 5
     }
 });
 
